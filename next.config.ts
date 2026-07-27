da5f1ba39630fa1892fbb92c70c675ffe64dafb3
mors-codes <morsmatias15@gmail.com>
2026-07-27 20:35:44 +0800

    Redirect vercel.app to custom domain

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,21 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: import.meta.dirname,
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "dm-workflows-lovat.vercel.app",
+          },
+        ],
+        destination: "https://dmworkflows.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
